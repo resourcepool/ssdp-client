2b0e038c9eb1fb9bface42c553922d51dcbcc407
lbarreau@takima.fr <lbarreau@takima.fr>
2023-12-14 20:51:48 +0100

    fix: #37

--- src/main/java/io/resourcepool/ssdp/exception/NoSerialNumberException.java
+++ src/main/java/io/resourcepool/ssdp/exception/NoSerialNumberException.java
@@ -1,9 +1,27 @@
 package io.resourcepool.ssdp.exception;
 
+import io.resourcepool.ssdp.client.response.SsdpResponse;
+
 /**
  * TODO class details.
  *
  * @author Loïc Ortola on 11/11/2017
  */
 public class NoSerialNumberException extends RuntimeException {
+
+    private SsdpResponse response;
+    public NoSerialNumberException() {
+
+    }
+    public NoSerialNumberException(SsdpResponse response) {
+        this.response = response;
+    }
+
+    @Override
+    public String toString() {
+        return "NoSerialNumberException{" +
+                "response=" + response +
+                '}';
+    }
+
 }
